ca213a6b5e452090bd48e5f1bb85c07b488d50ac
ShadyHippo <tim.vandyke123@gmail.com>
2025-06-24 15:30:07 -0400

    fix name etc

--- rpi-reboot-switch/setup.sh
+++ rpi-reboot-switch/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install python3-gpiozero
+echo "sudo apt update"
+echo "sudo apt upgrade -y"
+echo "sudo apt install python3-gpiozero"
 
 
 echo "run sudo raspi-config"
 echo "navigate to Interface Options"
 echo "  -> Enable SPI"
 echo "  -> Enable I2C"
-
+echo "reboot afterward"
